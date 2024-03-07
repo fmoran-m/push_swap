23e0c49ea33303a10b3435f4c469e66a0eaecf3e
Francisco Jose Moran Mato <fmoran-m@c2r7s6.42madrid.com>
2024-03-07 22:30:31 +0100

    Argc control

--- main.c
+++ main.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmoran-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fmoran-m <fmoran-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 22:19:00 by fmoran-m          #+#    #+#             */
-/*   Updated: 2024/03/07 22:19:34 by fmoran-m         ###   ########.fr       */
+/*   Updated: 2024/03/07 22:29:37 by fmoran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	return 0;
+	if (argc < 2)
+	{
+		ft_putendl_fd("Incorrect number of arguments", 2);
+		exit(1);
+	}
+	argv = NULL;
+	return (0);
 }
+
